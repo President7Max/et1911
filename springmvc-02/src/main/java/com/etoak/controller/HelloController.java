d82f1ae37c090d2206f2022c48a77c4a67fed873
et <et@Gay.com>
2020-05-19 15:01:37 +0800

    maven重构，提交springmvc-02和springmvc-03

---
+++ springmvc-02/src/main/java/com/etoak/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.etoak.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String name = request.getParameter("name");
+		
+		ModelAndView mv = new ModelAndView();
+		//webapp/pages/hello.jsp
+		mv.setViewName("hello");
+		
+		mv.addObject("result","Hello"+name);
+		
+		return mv;
+	}
+
+}
